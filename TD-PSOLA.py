0a28cb5d890770aa1c9405626b79b5102a82d3b8
Fonzie <675688853@qq.com>
2018-03-25 18:18:51 +0800

    Update TD-PSOLA.py

--- TD-PSOLA.py
+++ TD-PSOLA.py
@@ -12,7 +12,7 @@ N = math.ceil(num_samples/step_size)
 padding_num = int(N*step_size)-num_samples
 data = np.concatenate((data,np.array([0]*padding_num)))
 frames = np.zeros((N-1,step_size*2))
-w = 0.5-0.5*np.cos(2*np.pi*np.arange(0,2*step_size)/(2*step_size))
+w = 0.5-0.5*np.cos(2*np.pi*(np.arange(-step_size,step_size)+step_size)/(step_size-1))
 #plt.plot(w)
 #plt.show()
 for i in range(N-1):
